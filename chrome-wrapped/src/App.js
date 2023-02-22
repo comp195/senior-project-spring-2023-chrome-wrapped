c85addd466f426ec398505ccee44e963afd10ab0
projo11 <j_somers2@u.pacific.edu>
2023-02-22 10:31:45 -0800

    Created default layout

--- chrome-wrapped/src/App.js
+++ chrome-wrapped/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const appStyle = {
+    maxWidth: 1000,
+    margin: "auto",
+    paddingBottom: 50
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
     </div>
-  );
+  )
 }
+
+const Header = () => {
+  const headerStyle = {
+    margin: "auto",
+    width: "50%",
+    padding: 10,
+    textAlign: "center"
+  }
+  return (
+    <h1 style={headerStyle}>Chrome Wrapped(TM)</h1>
+  )
+}
+
 
 export default App;
