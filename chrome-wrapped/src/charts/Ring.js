daecb3254ea7b376c06a96698dfa260645d76074
projo11 <j_somers2@u.pacific.edu>
2023-03-01 12:05:28 -0800

    Added Ring chart

---
+++ chrome-wrapped/src/charts/Ring.js
@@ -0,0 +1,6 @@
+import Chart from "chart.js/auto";
+import { Doughnut } from "react-chartjs-2"
+
+const Ring = ({data}) => (<Doughnut data={ data }/>)
+
+export default Ring
--- chrome-wrapped/src/components/Body.js
+++ chrome-wrapped/src/components/Body.js
@@ -1,3 +1,4 @@
+import Ring from '../charts/Ring'
 
 const Body = ({ currentTab }) => {
     let renderText = 'text'
@@ -12,8 +13,28 @@ const Body = ({ currentTab }) => {
     console.log(currentTab)
     switch (currentTab) {
         case'Overview':
-            renderStyle = {...defaultRenderStyle, backgroundColor: "blue"}
-            renderText = 'Overview'
+        const data = {
+                labels: [
+                  'Red',
+                  'Blue',
+                  'Yellow'
+                ],
+                datasets: [{
+                  label: 'My First Dataset',
+                  data: [300, 50, 100],
+                  backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)'
+                  ],
+                  hoverOffset: 4
+                }]
+              }
+            return (
+                <Ring data={data}/>
+            )
+            //renderStyle = {...defaultRenderStyle, backgroundColor: "blue"}
+            //renderText = 'Overview'
             break
         case'Detailed':
             renderStyle = {...defaultRenderStyle, backgroundColor: "green"}
