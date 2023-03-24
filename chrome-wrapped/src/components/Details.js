53c16f4731ac9a68a59a539acea19333ad079814
projo11 <j_somers2@u.pacific.edu>
2023-03-24 11:10:55 -0700

    added search bar

--- chrome-wrapped/src/components/Details.js
+++ chrome-wrapped/src/components/Details.js
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 
-const Details = () => {
+const Details = (props) => {
+
+    const defaultSearch = props.search || ''
+
     const textStyle = {
         backgroundColor: "inherit",
         margin: "auto",
@@ -9,13 +12,28 @@ const Details = () => {
         display: "flex"
     }
 
+    const [filter, setFilter] = useState(defaultSearch)
+
+    useEffect(() => {
+        //Get the data from the API
+    }, [filter])
+
     return (
         <div>
-            <h3 style={textStyle}>Visit Counts: </h3>
+            <SearchBox filter={filter} setFilter={setFilter}/>
+            <h3 style={textStyle}>Visit Count: </h3>
             <h3 style={textStyle}>Last Time Visited: </h3>
             <h3 style={textStyle}>Most Recent URLs: </h3>
         </div>
     )
+}
+
+const SearchBox = ({filter, setFilter}) => {
+    const handleFilterChange = event => {
+        console.log(event.target.value)
+        setFilter(event.target.value)
+    }
+    return <p>URL contains: <input value={filter} onChange={handleFilterChange}/></p>
 }
 
 export default Details
