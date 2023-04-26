595ec1a3988bc731cf2860d4c335c261ab655dd8
mvu15 <m_vu12@u.pacific.edu>
2023-04-26 11:24:17 -0700

    changed the format and functionality of the links on About Tab

--- chrome-wrapped/src/components/About.js
+++ chrome-wrapped/src/components/About.js
@@ -1,16 +1,30 @@
-
+/* global chrome */
 
 const About = () => {
 
+    const openLink = (url) => {
+        chrome.tabs.create({
+            url: url,
+            active: true
+          })
+    }
+
     return (
         <div>
-            <h2 style={{color:"#3e42a7"}}><center>The Team</center></h2>
-            <h3><center>Sage Lee, Jordan Somers, Michelle Vu</center></h3>
-            <h2 style={{color:"#3e42a7"}}><center>About Senior Project</center></h2>
-            <h3 ><center>Chrome Wrapped enhances your browsing experience written in React.js</center></h3>
-            <h3 style={{color:"#3e42a7"}}><a href = 'https://forms.gle/7hnGy6iebANHyCTCA'><center>Report any bugs/feature requests through our request portal</center></a></h3>
-            <h3 style={{color:"#3e42a7"}}><a href = 'https://github.com/comp195/senior-project-spring-2023-chrome-wrapped'><center>View source code through github</center></a></h3>
+            <div>
+                <h2 style={{color:"#3e42a7"}}><center>The Team</center></h2>
+                <h3><center>Sage Lee, Jordan Somers, Michelle Vu</center></h3>
+                <h2 style={{color:"#3e42a7"}}><center>About Senior Project</center></h2>
+                <h3 ><center>Chrome Wrapped enhances your browsing experience written in React.js</center></h3>
+            </div>
+            <div className="container">
+                <h3 onClick={() => openLink('https://forms.gle/7hnGy6iebANHyCTCA')}><a href = 'https://forms.gle/7hnGy6iebANHyCTCA'><center>Report any bugs/feature requests</center></a></h3>
+            </div>
+            <div className="container">
+                <h3 onClick={() => openLink('https://github.com/comp195/senior-project-spring-2023-chrome-wrapped')}><a href = 'https://github.com/comp195/senior-project-spring-2023-chrome-wrapped'><center>View source code</center></a></h3>
+            </div>
 
+            
         </div>
 
     )
